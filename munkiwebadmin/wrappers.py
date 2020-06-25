50f7cb06760cf2a9da21c3f0b6aff94655f4fba0
Paul Suh <paul@mspex.net>
2020-06-25 15:21:31 -0400

    Decode plistlibs.dump() result to str from bytes

--- munkiwebadmin/wrappers.py
+++ munkiwebadmin/wrappers.py
@@ -91,7 +91,7 @@ def writePlist(data, filepath):
 def writePlistToString(data):
     '''Wrapper for the differences between Python 2 and Python 3's plistlib'''
     try:
-        return plistlib.dumps(data)
+        return plistlib.dumps(data).decode('utf-8')
     except AttributeError:
         # plistlib module doesn't have a dumps function (as in Python 2)
         try:
