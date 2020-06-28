11b858bd35b9f8c7ce4d59afd3bde15726ebff01
Paul Suh <plsuh@goodeast.com>
2020-06-28 01:14:13 +0000

    Update settings template to account for settings updates to Django 3.0.7 Inculde updated migrations file

--- munkiwebadmin/settings_template.py
+++ munkiwebadmin/settings_template.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django_wsgiserver',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
